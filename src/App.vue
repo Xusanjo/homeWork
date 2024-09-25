5be03433d8a98bf589ea7b180871b5f6fae6b2d3
Xusanjon <xusanmurodov907@gmail.com>
2024-09-25 23:23:45 +0500

    uy ishi

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import UserCard from "../src/components/UserCard.vue"
+export default {
+  component: {
+    UserCard
+  },
+  data() {
+    return {
+      userData: {}
+    }
+  },
+  methods: {
+    async getData(){
+      try {
+        const res = await fetch("https://dummyjson.com/users/1")
+        if(res.status === 200){
+          const user = await res.json()
+          this.userData = user.data
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  watch: {},
+  mounted() {
+  },
+}
+</script>
+
+<template>
+    <div class="container grid grid-cols-4">
+      <UserCard :user="userData">
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        user: Object,
+    },
+    methods: {
+        
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted() {
+        
+    },
+}
+</script>
+
+<template>
+    <div>
+        <pre>{{ user }}</pre>
+    </div>
+</template>
+
+<style>
+
+</style>
